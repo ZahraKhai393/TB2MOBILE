3fcb322e86ad9d618e8c5fdab3c1aaf39fdce1d2
Zahra Khairunnisha <168368575+ZahraKhai393@users.noreply.github.com>
2025-01-23 10:04:30 +0700

    Add files via upload

---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const baseColor = Color(0xff20223F);
+
+const second = Color(0xff009090);
+
+const baseUrl = 'https://2e69-119-235-221-138.ngrok-free.app';
